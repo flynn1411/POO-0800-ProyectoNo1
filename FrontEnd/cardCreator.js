92966943ead756c5126bc6621315611a299e4878
flynn1411 <flynn1411@gmail.com>
2020-03-04 21:23:46 -0600

    JSON commit

--- FrontEnd/cardCreator.js
+++ FrontEnd/cardCreator.js
@@ -2,8 +2,8 @@ function CardCreator(){
 
     this.createCard = function(card) {
         var body = document.querySelector("body");
-        var style = `height: 35vh; width: 10vw; font-size: 3rem; font-family: 'Fira Code', 
-                    monospace; line-height: 10vh; text-align: center; border-radius: 1rem; 
+        var style = `height: ${card.height}vh; width: ${card.width}vw; font-size: 3rem; font-family: 'Fira Code', 
+                    monospace; line-height: ${card.width*1.5}vh; text-align: center; border-radius: 1rem; 
                     background-color: #000000; color: ${card.color};`;
         var cardDIV = `<div id="card" style="${style}" >
                         <span style="vertical-align: middle; display: inline-block">
@@ -16,13 +16,15 @@ function CardCreator(){
 }
 
 
-function Card(symbol, color){
+function Card(symbol, color, width){
     this.symbol = symbol;
     this.color = color;
+    this.width = width;
+    this.height = width*(3.5)
 }
 
 function Randomizer(){
-    this.randomize = (max, min) => {
+    this.randomize = function(max, min){
         rand = Math.random();
         return parseInt( rand*(max-min) +min );
     };
