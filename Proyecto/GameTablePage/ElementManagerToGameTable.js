7a4591aa10abf31b6db2249cb9cfe98a386dd984
MrZombie1 <48537476+MrZombie1@users.noreply.github.com>
2020-03-18 10:45:37 +0800

    Add files via upload

--- Proyecto/GameTablePage/ElementManagerToGameTable.js
+++ Proyecto/GameTablePage/ElementManagerToGameTable.js
@@ -1,181 +1,257 @@
-/**
- * 
- */
+function ElementsManager(){
 
-var clickedCard = null;
-var tempArrPrincipalDeck = [fisrtCard,tempCard1,tempCard2,tempCard3,tempCard4,tempCard5,tempCard6,tempCard7,tempCard8,tempCard9,tempCard10]; //Arreglo que contendra las cartas en la baraja principal.
-var topCard = discardedDeck;
-var styleCss = `<style>
-                div.box{
-                    position: fixed;
-                    border: 2px solid black;
-                    width: 100vw;
-                    height: 33vh;
-                }
+    //Guarda la carta que se ha clickeado. 
+    var clickedCard,
 
-                div#areaP1{
-                    background-color: #40CC37;
-                    top: 0;
-                    left: 0;
-                    z-index:-1;
-                }
+    //Guarda todas las cartas (baraja principal).
+    tempArrPrincipalDeck = [tempCard1,tempCard2,tempCard3,tempCard4,
+                                tempCard5,tempCard6,tempCard7,tempCard8,
+                                tempCard9,tempCard10,tempCard11,tempCard12,
+                                tempCard13,tempCard14,tempCard15,tempCard16,
+                                tempCard17,tempCard18,tempCard19,tempCard20
+                            ];
 
-                div#areaTable{
-                    background-color: #D99486;
-                    bottom: 0;
-                    left: 0;
-                    z-index:-1;
-                }
+    //Contiene las cartas de la baraja del jugador 1.                        
+    tempArrDeckP1 = [],
 
-                div#areaP2{
-                    background-color: #427DCD;
-                    bottom: 0;
-                    right: 0;
-                    z-index:-1;
-                }
-                div#discardedDeck{
-                    border: 1px solid red;
-                    position: absolute;
-                    top:40vh;
-                    left:30vw;
-                    width:95px;
-                    height:135px;
-                    box-shadow: 1px 1px 1px #777, 0px 0px 10px 10px #3740B4;
-                }
-                div#principalDeck{
-                    border: 1px solid red;
-                    position: absolute;
-                    top:40vh;
-                    left:60vw;
-                    width:95px;
-                    height:135px;
-                    box-shadow: 1px 1px 1px #777, 0px 0px 10px 10px #3740B4;
-                }
-            `;
-//Funciones que se ejecutaran al iniciar el programa.
-function loadContent(){
+    //Contiene las cartas de la baraja del jugador 2.
+    tempArrDeckP2 = [],
     
-    this.distributeCards();
-}
-
-function eventT(element){
-    if(clickedCard == null){
-        clickedCard = element;
-        
-        //clickedCard.style.transform ="translateY(-60px)";
-        clickedCard.style.boxShadow = "1px 1px 1px #777, 0px 0px 30px 30px #B43B37";
-        
-    }else{
-        //console.log(clickedCard.style);
-        //clickedCard.style.transform = "traslateY(200px)";
-        clickedCard.style.boxShadow = "none";
-        clickedCard = element;
-        clickedCard.style.boxShadow = "1px 1px 1px #777, 0px 0px 30px 30px #B43B37";
-    }
-}
+    //Contiene las cartas de la baraja descartada.
+    discardedDeckArr = [],
     
-function eventDeck(){
-    if(clickedCard!=null){
-        var action = "TestValidateMovement.jsp";
-        var parameters = {
-            "player":1,
-            "from":"hisDesck",
-            "to":"discartedDeck",
-            "cards":{
-                    "player":{"color":"green","number":7},
-                    "deck":{"color":"green","number":3}    
+    //zIndex inicial.
+    currentzIndex = -299,
+    
+    //El turno actual.
+    currentTurn = 1,
+    
+    //Estilo inicial.
+    styleCss = `<style>
+                    div.box{
+                        position: fixed;
+                        border: 2px solid black;
+                        width: 100vw;
+                        height: 33vh;
                     }
-        
-            };
-        var callback = function(response){
-            //console.log(response)
-            if(response == "true"){
-                clickedCard.style.top = "40vh";
-                clickedCard.style.left = "30vw";
+    
+                    div#areaP1{
+                        background-color: #40CC37;
+                        top: 0;
+                        left: 0;
+                        z-index:-10000;
+                    }
+    
+                    div#areaTable{
+                        background-color: #D99486;
+                        bottom: 0;
+                        left: 0;
+                        z-index:-10000;
+                    }
+    
+                    div#areaP2{
+                        background-color: #427DCD;
+                        bottom: 0;
+                        right: 0;
+                        z-index:-10000;
+                    }
+                    div#discardedDeck{
+                        border: 1px solid red;
+                        position: absolute;
+                        top:40vh;
+                        left:30vw;
+                        width:95px;
+                        height:135px;
+                        box-shadow: 1px 1px 1px #777, 0px 0px 10px 10px #3740B4;
+                    }
+                    div#principalDeck{
+                        border: 1px solid red;
+                        position: absolute;
+                        top:40vh;
+                        left:60vw;
+                        width:95px;
+                        height:135px;
+                        box-shadow: 1px 1px 1px #777, 0px 0px 10px 10px #3740B4;
+                    }
+                `;
+    //Obtener el estilo.
+    this.getStyle = function(){
+        return styleCss;
+    }
+    //Evento al clickear en las cartas del jugador.
+    //Element es el div de la carta. Lo mismo si usara getElementById
+    this.eventT = function(element){
+        var sideOfClickedCard = tempArrDeckP1.includes(element);     //Verifica si el valor esta en el arreglo.
+        var playerCard;
+    
+        if(sideOfClickedCard == true){playerCard = 1;}
+        else{playerCard = 2;}
+    
+        if(currentTurn == playerCard){
+            
+            if(clickedCard == null){
+                clickedCard = element;
+                clickedCard.style.transition = "all 0.2s ease 0s"
+                clickedCard.style.boxShadow = "1px 1px 1px #777, 0px 0px 30px 30px #B43B37";
+                
+            }else{
                 clickedCard.style.boxShadow = "none";
-                clickedCard = null;
+                clickedCard = element;
+                clickedCard.style.transition = "all 0.2s ease 0s"
+                clickedCard.style.boxShadow = "1px 1px 1px #777, 0px 0px 30px 30px #B43B37";
             }
-            else{
-                console.log("MOVIMIENTO INVALIDO");
-            }
-        }    
+    
+        }
+    }
         
-        $.get(action,parameters,callback);
-
-
-    } 
+    //Evento al clickear en la carta encima de la baraja descartada.
+    this.eventDeck = function(){
     
+        if(clickedCard != null){
+            var action = "TestValidateMovement.jsp";
+            var parameters = {
+                "player":1,
+                "from":"hisDesck",
+                "to":"discartedDeck",
+                "cards":{
+                        "player":{"color":"green","number":7},
+                        "deck":{"color":"green","number":3}    
+                        }
+                };
     
-}
-//Reparte las cartas al inicio, la cantidad correspondiente a cada jugador.
-//Dejando una carta en el area de cardas descartadas.
-function distributeCards(){
-    //console.log("Ha dobleclickeado la baraja principal");
-    var valueLeft = 20;
-    for(let i=0;i<5;i++){
-        //console.log(i);
-        //console.log(tempArrPrincipalDeck.length)
-       
-        if(i == 4){
-            console.log("Entro");
-            tempArrPrincipalDeck[i].style.top ="38vh";
-            tempArrPrincipalDeck[i].style.left = "30vw";        
-            tempArrPrincipalDeck[i].style.zIndex = "-2";
+            var callback = function(response){
+                if(response == "true"){
+                    //console.log("ZIndex de la carta clickeada: " + currentzIndex);
+                    clickedCard.style.zIndex = `${currentzIndex}`;
+                    clickedCard.style.top = "40vh";
+                    clickedCard.style.left = "30vw";
+                    clickedCard.style.boxShadow = "none";
+                    clickedCard.style.transition = "all 0.2s ease 0s"
+                    clickedCard = null;
+                    
+                }
+                else{
+                    //Cuando un moviminento no es validado.
+                    console.log("MOVIMIENTO INVALIDO");
+                }
+            }    
+            
+            $.get(action,parameters,callback);
+    
+            
         }else{
-            tempArrPrincipalDeck[i].style.top ="70vh";
-            tempArrPrincipalDeck[i].style.left =`${valueLeft}vw`;
-            valueLeft = valueLeft + 10;    
+            console.log("No hay carta clickeada.")
         }
-
-    }
-}
-
-function createCard(elementID,top,left,urlImage,zIndex){
-
-    styleCss += `
-    .${elementID} {    
-        opacity: 1;
-        background-image: url("${urlImage}");
-        border-radius: 12px;
-        background-size: 90px 130px;
-        border: solid black ;
-        border-radius: 12px;
-        cursor: pointer;
-        display: block;  
-        width: 50px;
-        height: 90px;
-        padding: 20px 20px;
-        transition: all 0.2s ease 0s;
-        position: absolute;
-        top: ${top}vh;
-        left: ${left}vw;
-        z-index:${zIndex};
-    }
-    `;
-}
-function editProperties(elementID){
-    styleCss += `
-    .${elementID}{
-        opacity: 1;
-        background-image: url("../Images/back2.jpg");
-        border-radius: 12px;
-        background-size: 90px 130px;
-        border: solid black ;
-        border-radius: 12px;
-        cursor: pointer;
-        display: block;  
-        width: 50px;
-        height: 90px;
-        padding: 20px 20px;
-        transition: all 0.2s ease 0s;
-        position: absolute;
-        top: 70vh;
-        left: 15vw;
-    }    
-        `;    
     
-        }
-
-    function fishStyle(){
-        styleCss +=`</style>`;
+        //Al terminar de colocar carta, se cambia turno.
+        if(currentTurn == 1){currentTurn = 2;}
+        else{currentTurn = 1;}
+        
+        currentzIndex = currentzIndex + 1; 
     }
+    
+    //Reparte las cartas al inicio, la cantidad correspondiente a cada jugador.
+    //Dejando una carta en el area de cardas descartadas.
+    this.distributeCards = function(){
+        //setDistributeAnimation();
+        var valueMoveP1 = 20,
+            valueMoveP2 = 20,
+            valueDelayDistributed = 0.5;
+        
+        //Reparte 7 cartas al jugador 1.
+        for(let i=0;i<=6;i++){
+                tempArrPrincipalDeck[i].style.animation = "distributeCards .8s ease .5s forwards";
+                tempArrPrincipalDeck[i].style.animationDelay = `${valueDelayDistributed}s`;
+                tempArrPrincipalDeck[i].style.animationDuration = "0.5s";    
+                tempArrPrincipalDeck[i].style.transition = `all ${valueDelayDistributed}s ease 0s`;
+                tempArrPrincipalDeck[i].style.top = "70vh";
+                tempArrPrincipalDeck[i].style.left =`${valueMoveP1}vw`;
+                
+                valueMoveP1 = valueMoveP1 + 10;
+                valueDelayDistributed =valueDelayDistributed + 0.3;
+    
+                tempArrDeckP1.push(tempArrPrincipalDeck[i]);
+            }
+    
+            //Reparte 7 cartas al jugador 1.
+            for (let i=7;i<=14;i++){
+                if(i == 14){
+                    tempArrPrincipalDeck[i].style.animation = "distributeCards .8s ease .5s forwards";
+                    tempArrPrincipalDeck[i].style.animationDelay = `${valueDelayDistributed-2}s`;
+                    tempArrPrincipalDeck[i].style.animationDuration = "0.5s";    
+                    tempArrPrincipalDeck[i].style.transition = `all ${valueDelayDistributed-1.5}s ease 0s`;
+    
+                    tempArrPrincipalDeck[i].style.top ="38vh";
+                    tempArrPrincipalDeck[i].style.left = "30vw";        
+                    tempArrPrincipalDeck[i].style.zIndex = "-300";
+    
+                    discardedDeckArr.push(tempArrPrincipalDeck[i]);
+                }else{
+                    
+                    tempArrPrincipalDeck[i].style.animationDelay = `${valueDelayDistributed}s`;
+                    tempArrPrincipalDeck[i].style.animationDuration = "0.5s";    
+                    tempArrPrincipalDeck[i].style.transition = `all ${valueDelayDistributed-2}s ease 0.5s`;
+                    tempArrPrincipalDeck[i].style.top ="10vh";
+                    tempArrPrincipalDeck[i].style.left =`${valueMoveP2}vw`;
+                    
+                    valueMoveP2 = valueMoveP2 + 10;
+                    valueDelayDistributed =valueDelayDistributed + 0.3;     
+                    tempArrDeckP2.push(tempArrPrincipalDeck[i]);
+                }
+        }
+        //Se eliminan del mazo principal las 15 cartas repartidas.
+        for (let i=0;i<=14;i++){
+            tempArrPrincipalDeck.shift();
+        }
+    
+        //console.log("Tamano baraja principal: " + tempArrPrincipalDeck.length);
+        //console.log("Tamano baraja P1: " + tempArrDeckP1.length);
+        //console.log("Tamano baraja P2: " + tempArrDeckP2.length);
+        //console.log("Tamano baraja descartada: " + discardedDeckArr.length);
+        
+    }
+    //Funcion para crear cartas.
+    this.createCard = function(elementID,top,left,urlImage){
+    
+        styleCss += `
+        .${elementID} {    
+            opacity: 1;
+            background-image: url("${urlImage}");
+            border-radius: 12px;
+            background-size: 90px 130px;
+            border: solid black ;
+            border-radius: 12px;
+            cursor: pointer;
+            display: block;  
+            width: 50px;
+            height: 90px;
+            padding: 20px 20px;
+            transition: all 0.2s ease 0s;
+            position: absolute;
+            top: ${top}vh;
+            left: ${left}vw;
+        }
+        `;
+    }
+    
+    //Define la animacion de distribucion.
+    this.setDistributeAnimation = function(){
+        styleCss += `
+    
+            @keyframes distributeCards {
+                from { 
+                    opacity: 1;
+                } 
+            
+                to {
+                    transform: rotateY(180deg);    
+                    opacity: 1; 
+                }
+            }
+        `;
+    }
+        //Finaliza el agregado de estilo.
+    this.fishStyle = function(){
+            styleCss +=`</style>`;
+        }
+}
--- Proyecto/GameTablePage/GameTablePage.jsp
+++ Proyecto/GameTablePage/GameTablePage.jsp
@@ -28,6 +28,16 @@
     <div id="tempCard8" class="tempCard8" onClick="eventT(this);"></div>
     <div id="tempCard9" class="tempCard9" onClick="eventT(this);"></div>
     <div id="tempCard10" class="tempCard10" onClick="eventT(this);"></div>
+    <div id="tempCard11" class="tempCard11" onClick="eventT(this);"></div>
+    <div id="tempCard12" class="tempCard12" onClick="eventT(this);"></div>
+    <div id="tempCard13" class="tempCard13" onClick="eventT(this);"></div>
+    <div id="tempCard14" class="tempCard14" onClick="eventT(this);"></div>
+    <div id="tempCard15" class="tempCard15" onClick="eventT(this);"></div>
+    <div id="tempCard16" class="tempCard16" onClick="eventT(this);"></div>
+    <div id="tempCard17" class="tempCard17" onClick="eventT(this);"></div>
+    <div id="tempCard18" class="tempCard18" onClick="eventT(this);"></div>
+    <div id="tempCard19" class="tempCard19" onClick="eventT(this);"></div>
+    <div id="tempCard20" class="tempCard20" onClick="eventT(this);"></div>
 
     <div id="discardedDeck" class="discardedDeck" onClick="eventDeck()"></div>
     <div id="principalDeck" class="principalDeck" ondblclick="eventPrincipalDeck(this)"></div>
@@ -36,23 +46,42 @@
     <script src="ElementManagerToGameTable.js"></script>
     <script src="../../jquery.js"></script>
     <script>
+        var em = new ElementsManager();
+        window.onload = em.distributeCards;
+        /*
+        function eventT(element) {
+            em.eventT(element);
+        }
+        function eventDeck(){
+            em.eventDeck();
+        }
 
-        window.onload = distributeCards;
+        em.createCard("tempCard1","40","60","../Images/back2.jpg");
+        em.createCard("tempCard2","40","60","../Images/back.jpg");
+        em.createCard("tempCard3","40","60","../Images/reverseTransp3.jpg");
+        em.createCard("tempCard4","40","60","../Images/score.jpg");
+        em.createCard("tempCard5","40","60","../Images/prueba.jpg");
+        em.createCard("tempCard6","40","60","../Images/score.jpg");
+        em.createCard("tempCard7","40","60","../Images/back2.jpg");
+        em.createCard("tempCard8","40","60","../Images/uno.png");
+        em.createCard("tempCard9","40","60","../Images/uno.png");
+        em.createCard("tempCard10","40","60","../Images/uno.png");
+        em.createCard("tempCard11","40","60","../Images/uno.png");
+        em.createCard("tempCard12","40","60","../Images/uno.png");
+        em.createCard("tempCard13","40","60","../Images/uno.png");
+        em.createCard("tempCard14","40","60","../Images/uno.png");
+        em.createCard("tempCard15","40","60","../Images/back2.jpg");
+        em.createCard("tempCard16","40","60","../Images/uno.png");
+        em.createCard("tempCard17","40","60","../Images/uno.png");
+        em.createCard("tempCard18","40","60","../Images/uno.png");
+        em.createCard("tempCard19","40","60","../Images/uno.png");
+        em.createCard("tempCard20","40","60","../Images/uno.png");
 
-        createCard("fisrtCard","40","60","../Images/especial.jpg","-1");
-        createCard("tempCard1","40","60","../Images/back2.jpg","-1");
-        createCard("tempCard2","40","60","../Images/back.jpg","-1");
-        createCard("tempCard3","40","60","../Images/reverseTransp3.jpg","-1");
-        createCard("tempCard4","40","60","../Images/score.jpg","-1");
-        createCard("tempCard5","40","60","../Images/especial.jpg","-1");
-        createCard("tempCard6","40","60","../Images/especial.jpg","-1");
-        createCard("tempCard7","40","60","../Images/especial.jpg","-1");
-        createCard("tempCard8","40","60","../Images/especial.jpg","-1");
-        createCard("tempCard9","40","60","../Images/especial.jpg","-1");
-        createCard("tempCard10","40","60","../Images/especial.jpg","-1");
-
-        fishStyle();
-        style.innerHTML =  styleCss;
+        em.setDistributeAnimation();
+        */
+        em.fishStyle();
+        
+        style.innerHTML =  em.getStyle();
     </script>
 
 </body>
